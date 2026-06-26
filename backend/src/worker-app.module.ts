ff481efdf4eab9707956218f81fcd0399673c557
lexicorelabssystem <lexicorelabssystem@gmail.com>
2026-06-26 01:04:40 -0400

    Fix worker NotificationsModule dependency

--- backend/src/worker-app.module.ts
+++ backend/src/worker-app.module.ts
@@ -3,8 +3,9 @@ import { ConfigModule } from "./config/config.module.js";
 import { PrismaModule } from "./modules/prisma/prisma.module.js";
 import { QueueModule } from "./modules/queue/queue.module.js";
 import { WorkersModule } from "./modules/workers/workers.module.js";
+import { NotificationsModule } from "./modules/notifications/notifications.module.js";
 
 @Module({
-  imports: [ConfigModule, PrismaModule, QueueModule, WorkersModule],
+  imports: [ConfigModule, PrismaModule, QueueModule, WorkersModule, NotificationsModule],
 })
 export class WorkerAppModule {}
